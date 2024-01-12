f01f6e3a849ce5fb3d1e8a1ca25bde86e484f75e
bar181 <bar181@yahoo.com>
2024-01-12 16:09:59 -0500

    hello world vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Import the CSS for Tailwind
+import './assets/styles.css'; // Adjust the path as per your project structure
+
+// Import FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCoffee  } from '@fortawesome/free-solid-svg-icons';
+library.add(faUserSecret, faCoffee );
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+
+app.mount('#app');
+
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    // Add paths to all template files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
